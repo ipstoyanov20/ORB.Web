fd6ce19b1099edc39ef7f924d4eab22efd0caaf3
ISBachvarov21 <ISBachvarov21@codingburgas.bg>
2023-07-06 16:15:21 +0300

    Add a basic login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  let userdata:any = {
+    email: String,
+    password: String
+  };
+  
+  function onChangeEmail(e: React.FormEvent<HTMLInputElement>):void {
+    userdata.email = e.currentTarget.value;
+  };
+
+  function onChangePassword(e: React.FormEvent<HTMLInputElement>):void {
+    userdata.password = e.currentTarget.value;
+  };
+
+  function submit() {
+    alert(`${userdata.email} ${userdata.password}`);
+    // api call
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="login">
+        <input type="email" onChange = {onChangeEmail} placeholder="Enter email"></input>
+        <input type="password" onChange = {onChangePassword} placeholder="Enter password"></input>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <button onClick={submit}>Login</button>
+    </>
+  );
+  
+};
+
+export default App;
