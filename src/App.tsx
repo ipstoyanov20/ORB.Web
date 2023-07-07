3b173ca0d753396cd9caf15db69db228f1bd33f5
ISBachvarov21 <ISBachvarov21@codingburgas.bg>
2023-07-07 11:12:39 +0300

    Add register
    transition login and register forms into different states

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,96 @@
 import React from 'react';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
-  let userdata:any = {
-    email: String,
-    password: String
-  };
+  const [count, setCount] = useState(0);
   
-  function onChangeEmail(e: React.FormEvent<HTMLInputElement>):void {
-    userdata.email = e.currentTarget.value;
-  };
+  if (count === 0) {
+    let userdata:any = {
+      email: String,
+      password: String
+    };
 
-  function onChangePassword(e: React.FormEvent<HTMLInputElement>):void {
-    userdata.password = e.currentTarget.value;
-  };
-
-  function submit() {
-    alert(`${userdata.email} ${userdata.password}`);
-    // api call
-  };
-
-
-  return (
-    <>
-      <div className="login">
-        <input className='m-3' type="email" onChange = {onChangeEmail} placeholder="Enter email"></input>
-        <input className='m-3' type="password" onChange = {onChangePassword} placeholder="Enter password"></input>
-      </div>
-
-      <button className='text-[#646cff] bg-black' onClick={submit}>Login</button>
-    </>
-  );
+    function onChangeEmail(e: React.FormEvent<HTMLInputElement>):void {
+      userdata.email = e.currentTarget.value;
+    };
   
+    function onChangePassword(e: React.FormEvent<HTMLInputElement>):void {
+      userdata.password = e.currentTarget.value;
+    };
+  
+    function submitLogin() {
+      alert(`Login: ${userdata.email} ${userdata.password}`);
+      // api call
+    };
+  
+  
+    return (
+      <>
+        <div className="inputDiv">
+          <input className = "m-3" type="email" onChange = {onChangeEmail} placeholder="Email"></input>
+          <input className = "m-3" type="password" onChange = {onChangePassword} placeholder="Password"></input>
+        </div>
+  
+        <div className="buttons">
+          <button className='text-[#646cff] bg-black' onClick={submitLogin}>Login</button>
+          <div className="break"></div>
+          <button className='text-[#646cff] bg-black' onClick={() => {
+            setCount((preval: number):number => preval + 1);
+          }}>state++</button>
+        </div>
+      </>
+    );
+  }
+  else if (count === 1) {
+    let userdata:any = {
+      fName: String,
+      lName: String,
+      email: String,
+      password: String
+    };
+
+    function onChangeEmail(e: React.FormEvent<HTMLInputElement>): void {
+      userdata.email = e.currentTarget.value;
+    };
+  
+    function onChangePassword(e: React.FormEvent<HTMLInputElement>): void {
+      userdata.password = e.currentTarget.value;
+    };
+
+    function onChangeFName(e: React.FormEvent<HTMLInputElement>): void {
+      userdata.fName = e.currentTarget.value;
+    };
+
+    function onChangeLName(e: React.FormEvent<HTMLInputElement>): void {
+      userdata.lName = e.currentTarget.value;
+    }
+  
+    function submitRegister() {
+      alert(`Register: ${userdata.fName} ${userdata.lName} ${userdata.email} ${userdata.password}`);
+      // api call
+    };
+
+    return (
+      <>
+        <div className="inputDiv">
+          <input className = "m-3" type="text" onChange = {onChangeFName} placeholder="First name"></input>
+          <input className = "m-3" type="text" onChange = {onChangeLName} placeholder="Last name"></input>
+          <input className = "m-3" type="email" onChange = {onChangeEmail} placeholder="Email"></input>
+          <input className = "m-3" type="password" onChange = {onChangePassword} placeholder="Password"></input>
+
+        </div>
+
+        <div className="buttons">
+          <button className='text-[#646cff] bg-black' onClick={submitRegister}>Register</button>
+          <button className='text-[#646cff] bg-black' onClick={() => {
+            setCount((preval: number):number => preval - 1);
+          }}>state--</button>
+        </div>
+      </>
+    )
+  }
+
 };
 
 export default App;
