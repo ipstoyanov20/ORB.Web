c8d79afda9e1d65c516f8a8087cf1b9b1da2b6f7
RayRay-0 <ivanovaraq11@gmail.com>
2023-07-10 12:17:01 +0300

    Add login design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,45 +1,50 @@
 import { Form } from "react-router-dom";
 
 function Signin() {
-    let userdata:any = {
-        username: String,
-        password: String
-      };
-      
-      function onChangeUserName(e: React.FormEvent<HTMLInputElement>):void {
-        userdata.username = e.currentTarget.value;
-      };
-    
-      function onChangePassword(e: React.FormEvent<HTMLInputElement>):void {
-        userdata.password = e.currentTarget.value;
-      };
-    
-      function submit() {
-        alert(`${userdata.username} ${userdata.password}`);
-        // api call
-      };
-    
-    return (
-      <>
+  let userdata: any = {
+    username: String,
+    password: String
+  };
+
+  function onChangeUserName(e: React.FormEvent<HTMLInputElement>): void {
+    userdata.username = e.currentTarget.value;
+  };
+
+  function onChangePassword(e: React.FormEvent<HTMLInputElement>): void {
+    userdata.password = e.currentTarget.value;
+  };
+
+  function submit() {
+    alert(`${userdata.username} ${userdata.password}`);
+    // api call
+  };
+
+  return (
+    <>
+      <div className="grid w-full p-10 bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+        <h1 className="mb-5 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Sign in
+        </h1>
         <Form className="grid">
           <input
-            className="m-3"
+            className="m-3 rounded text-slate-500 font-bold"
             type="email"
             onChange={onChangeUserName}
             placeholder=" Enter username"
           ></input>
           <input
-            className="m-3"
+            className="m-3 rounded text-slate-500 font-bold"
             type="password"
             onChange={onChangePassword}
             placeholder=" Enter password"
           ></input>
         </Form>
 
-        <button className="text-[#646cff] bg-black" onClick={submit}>
+        <button className="m-3 text-[#646cff] bg-black hover:bg-indigo-500 hover:text-slate-950" onClick={submit}>
           Sign in to your account
         </button>
-      </>
-    );
+      </div>
+    </>
+  );
 }
 export default Signin;
