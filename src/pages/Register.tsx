7ec83343d10b8640aecaa548863bba30185bfb33
ISBachvarov21 <ISBachvarov21@codingburgas.bg>
2023-07-14 20:41:33 +0300

    Make an attempt at connecting register to the api

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import authenticationService from "../services/authentication-service";
+
 function Signup()
 {
     let userdata:any = {
-        username: String,
+        firstName: String,
+        lastName: String,
         email: String,
         password: String,
       };
@@ -15,40 +18,58 @@ function Signup()
         userdata.password = e.currentTarget.value;
       };
 
-      function onChangeUserName(e: React.FormEvent<HTMLInputElement>):void {
-        userdata.username = e.currentTarget.value;
+      function onChangeFirstName(e: React.FormEvent<HTMLInputElement>):void {
+        userdata.firstName = e.currentTarget.value;
+      };
+
+      function onChangeLastName(e: React.FormEvent<HTMLInputElement>):void {
+        userdata.lastName = e.currentTarget.value;
       };
     
       function submit() {
         // api call for register
         // get user info
-        window.localStorage.setItem("username", userdata.username);
+        let code = authenticationService.makeRegisterRequest(userdata.firstName, userdata.lastName, userdata.email, userdata.password); // this doesn't work
+        console.log(code);
       };
     
     return (
       <>
         <div className="grid w-full p-10 bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
-              <h1 className="mb-5 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign up
-              </h1>
+          <h1 className="mb-5 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign up
+          </h1>
+
           <input
             className="m-3 rounded text-slate-500 font-bold"
             type="text"
-            onChange={onChangeUserName}
-            placeholder="Enter username"
-          ></input>
+            onChange={onChangeFirstName}
+            placeholder="Enter first name"
+          >
+          </input>
+
+          <input className="m-3 rounded text-slate-500 font-bold"
+            type="text"
+            onChange={onChangeLastName}
+            placeholder="Enter last name"
+          >
+          </input>
+
           <input
             className="m-3 rounded text-slate-500 font-bold"
             type="email"
             onChange={onChangeEmail}
             placeholder="Enter email"
-          ></input>
-           <input
+          >
+          </input>
+
+          <input
             className="m-3 rounded text-slate-500 font-bold"
             type="password"
             onChange={onChangePassword}
             placeholder="Enter password"
-          ></input>
+          >
+          </input>
           
           <Link to='/panel' onClick={submit} className="m-3 text-[#646cff] bg-black hover:bg-indigo-500 hover:text-slate-950 p-2.5 ">
               Sign up
