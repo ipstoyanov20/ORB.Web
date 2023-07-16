6af681ed4dc2dce1d1b7d0321bb99021ece930e0
ISBachvarov21 <ISBachvarov21@codingburgas.bg>
2023-07-16 21:26:35 +0300

    Update api call path

--- src/api/base.ts
+++ src/api/base.ts
@@ -16,7 +16,7 @@ import { Configuration } from "./configuration";
 // @ts-ignore
 import globalAxios, { AxiosRequestConfig, AxiosInstance } from 'axios';
 
-export const BASE_PATH = "http://localhost:7206".replace(/\/+$/, "");
+export const BASE_PATH = "http://localhost:5109".replace(/\/+$/, "");
 
 /**
  *
