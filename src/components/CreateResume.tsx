6de5c157c661be310d6abc13f1239644d2acb662
RayRay-0 <ivanovaraq11@gmail.com>
2023-07-20 19:28:51 +0300

    Add url change button

--- src/components/CreateResume.tsx
+++ src/components/CreateResume.tsx
@@ -8,7 +8,7 @@ const CreateResume = () => {
           alt=""
           className="absolute h-[100%] w-[50%] top-0 right-0 z-[-1]"
         />
-        <div className=" absolute top-0 left-0 grid place-items-center w-[50%] h-[100%]">
+        <div className="absolute top-0 left-0 grid place-items-center w-[50%] h-[100%]">
           <p className="bg-black rounded-md p-10 w-[50%]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
             excepturi quia minima rerum. Sapiente ullam repudiandae corrupti
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from 'react-router-dom';
 function Sidebar() {
     return (
       <>
-        <div className='z-[-1] absolute right-0 top-0 bg-gray-200 w-[80%] h-[100%]'></div>
+        <div className='z-[-1] fixed right-0 top-0 bg-gray-200 w-[100%] h-[100%]'></div>
 
         <div className="grid place-content-start shadow-2xl fixed left-0 top-0 p-5 w-[20%] h-[100%] bg-gradient-to-br from-gray-200 to-gray-500">
          
@@ -22,7 +22,7 @@ function Sidebar() {
             </Link>
             <Link
               className=" relative p-4 text-left text-white bg-gray-700 hover:bg-gray-600"
-              to="/panel/templates"
+              to="/panel/template0"
             >
               Templates
             </Link>
